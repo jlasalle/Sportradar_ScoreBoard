d5cbd162e4f1ab9ba56c74a8be5cd6279cdcd243
Jonathan Lasalle <johnlasalle@gmail.com>
2024-05-22 15:31:03 +0200

    [Core] Initialization of ScoreBoard display

---
+++ org.lasalle.scoreboard/src/org/lasalle/scoreboard/ScoreBoardDisplay.java
@@ -0,0 +1,27 @@
+package org.lasalle.scoreboard;
+
+/**
+ * Visualization of the ScoreBoard data
+ * 
+ * @author Jonathan Lasalle
+ */
+public class ScoreBoardDisplay {
+
+	/**
+	 * Display management constructor
+	 * 
+	 * @param scoreBoard Concerned scoreBoard
+	 */
+	public ScoreBoardDisplay(ScoreBoard scoreBoard) {
+	}
+
+	/**
+	 * Generate the summary
+	 * 
+	 * @return
+	 */
+	public String genSummary() {
+		return "";
+	}
+
+}
