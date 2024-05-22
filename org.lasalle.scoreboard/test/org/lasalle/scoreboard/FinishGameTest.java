57f41f4989838137002ad2a92596708d8adb7c43
Jonathan Lasalle <johnlasalle@gmail.com>
2024-05-22 13:00:52 +0200

    [Test] Finish a game: check the game is well removed

---
+++ org.lasalle.scoreboard/test/org/lasalle/scoreboard/FinishGameTest.java
@@ -0,0 +1,44 @@
+package org.lasalle.scoreboard;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+import org.lasalle.scoreboard.game.Game;
+
+/**
+ * Tests dedicated to the "Finish Game" feature. Remove a game from the
+ * scoreboard.
+ * 
+ * @author Jonathan Lasalle
+ */
+public class FinishGameTest {
+
+	/**
+	 * A_TEAM: test football team
+	 */
+	private static final String A_TEAM = "A_TEAM";
+
+	/**
+	 * B_TEAM: test football team
+	 */
+	private static final String B_TEAM = "B_TEAM";
+
+	/**
+	 * Test that the "finish game" feature effectively remove the game from the
+	 * score board
+	 */
+	@Test
+	public void test() {
+		ScoreBoardBuilder sbBuilder = new ScoreBoardBuilder();
+		ScoreBoard scoreBoard = sbBuilder.createScoreBoard();
+
+		ScoreBoardController sbCtrl = new ScoreBoardController(scoreBoard);
+		Game g = sbCtrl.startGame(A_TEAM, B_TEAM);
+
+		sbCtrl.finishGame(g);
+		
+		assertFalse(sbCtrl.containsGame(g));
+	}
+
+}
