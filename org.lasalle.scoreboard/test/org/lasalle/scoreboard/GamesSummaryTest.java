8a89b3dabef67b51412b18ebbf4c2df69e22c765
Jonathan Lasalle <johnlasalle@gmail.com>
2024-05-22 17:06:14 +0200

    [Test] ScoreBoard display: same score case

--- org.lasalle.scoreboard/test/org/lasalle/scoreboard/GamesSummaryTest.java
+++ org.lasalle.scoreboard/test/org/lasalle/scoreboard/GamesSummaryTest.java
@@ -35,6 +35,16 @@ public class GamesSummaryTest {
 	private static final String D_TEAM = "D_TEAM";
 
 	/**
+	 * E_TEAM: test football team
+	 */
+	private static final String E_TEAM = "E_TEAM";
+
+	/**
+	 * F_TEAM: test football team
+	 */
+	private static final String F_TEAM = "F_TEAM";
+
+	/**
 	 * Default Score
 	 */
 	private static final int DEFAULT_SCORE = 0;
@@ -69,6 +79,7 @@ public class GamesSummaryTest {
 		ScoreBoard scoreBoard = sbBuilder.createScoreBoard();
 		ScoreBoardController ctrl = new ScoreBoardController(scoreBoard);
 		ScoreBoardDisplay dis = new ScoreBoardDisplay(ctrl);
+		ScoreBoardScoreController scoreCtrl = new ScoreBoardScoreController();
 
 		// No games case
 		String sum = dis.genSummary();
@@ -76,7 +87,6 @@ public class GamesSummaryTest {
 
 		// Single game case
 		Game game = ctrl.startGame(A_TEAM, B_TEAM);
-		ScoreBoardScoreController scoreCtrl = new ScoreBoardScoreController();
 		scoreCtrl.updateScore(game, A_TEAM_NEW_SCORE, B_TEAM_NEW_SCORE);
 
 		sum = dis.genSummary();
@@ -97,10 +107,9 @@ public class GamesSummaryTest {
 		ScoreBoardDisplay dis = new ScoreBoardDisplay(ctrl);
 		ScoreBoardScoreController scoreCtrl = new ScoreBoardScoreController();
 
-		
-		//First Check
-		//A 1 - B 2
-		//C 0 - D 0
+		// First Check
+		// A 1 - B 2
+		// C 0 - D 0
 		Game abGame = ctrl.startGame(A_TEAM, B_TEAM);
 		scoreCtrl.updateScore(abGame, A_TEAM_NEW_SCORE, B_TEAM_NEW_SCORE);
 
@@ -111,13 +120,43 @@ public class GamesSummaryTest {
 				+ genExpectedSumLine(2, C_TEAM, DEFAULT_SCORE, D_TEAM, DEFAULT_SCORE);
 		assertEquals(expectedRes, sum);
 
-		//Second Check
-		//C 3 - D 4
-		//A 1 - B 2
+		// Second Check
+		// C 3 - D 4
+		// A 1 - B 2
 		scoreCtrl.updateScore(cdGame, C_TEAM_NEW_SCORE, D_TEAM_NEW_SCORE);
 		sum = dis.genSummary();
 		expectedRes = genExpectedSumLine(1, C_TEAM, C_TEAM_NEW_SCORE, D_TEAM, D_TEAM_NEW_SCORE)
 				+ genExpectedSumLine(2, A_TEAM, A_TEAM_NEW_SCORE, B_TEAM, B_TEAM_NEW_SCORE);
+		assertEquals(expectedRes, sum);
+	}
+
+	/**
+	 * Check that the "games summary" feature return the right String expression. 3
+	 * games with the same total: the most recent must be displayed first.
+	 */
+	@Test
+	public void gamesSummaryTest_equalityCase() {
+		ScoreBoardBuilder sbBuilder = new ScoreBoardBuilder();
+		ScoreBoard scoreBoard = sbBuilder.createScoreBoard();
+		ScoreBoardController ctrl = new ScoreBoardController(scoreBoard);
+		ScoreBoardDisplay dis = new ScoreBoardDisplay(ctrl);
+
+		// Created data
+		// A 0 - B 0
+		// C 0 - D 0
+		// E 0 - F 0
+		ctrl.startGame(A_TEAM, B_TEAM);
+		ctrl.startGame(C_TEAM, D_TEAM);
+		ctrl.startGame(E_TEAM, F_TEAM);
+
+		// Expected result
+		// E 0 - F 0
+		// C 0 - D 0
+		// A 0 - B 0		
+		String sum = dis.genSummary();
+		String expectedRes = genExpectedSumLine(1, E_TEAM, DEFAULT_SCORE, F_TEAM, DEFAULT_SCORE)
+				+ genExpectedSumLine(2, C_TEAM, DEFAULT_SCORE, D_TEAM, DEFAULT_SCORE)
+				+ genExpectedSumLine(3, A_TEAM, DEFAULT_SCORE, B_TEAM, DEFAULT_SCORE);
 		assertEquals(expectedRes, sum);
 	}
 
